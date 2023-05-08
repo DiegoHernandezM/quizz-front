fb62ea7f0b73bab971f583628866bd35c20f8c91
Diego Hernandez <diegohernandez@Diegos-MacBook-Air.local>
2023-05-08 10:55:57 -0600

    firts commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import "react-app-polyfill/stable";
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import "chart.js/auto";
+
+import App from "./App";
+import reportWebVitals from "./utils/reportWebVitals";
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+// Note: Remove the following line if you want to disable the API mocks.
+import "./mocks";
+
+import "animate.css/animate.min.css";
+import { AuthProvider } from "./contexts/JWTContext";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <ThemeProvider>
+      <AuthProvider>
+      <App />
+      </AuthProvider>
+    </ThemeProvider>
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
