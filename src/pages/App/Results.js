a92bc95c1a1e26691a8edda68f980d823f4fdc9e
AlvaroCarrillo <alvarocarrillo220@gmail.com>
2023-06-06 17:19:04 -0600

    progreso del usuario

--- src/pages/App/Results.js
+++ src/pages/App/Results.js
@@ -1,32 +1,60 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "@emotion/styled";
+import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import { getUserTests } from "../../redux/slices/usertests";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, esES } from "@mui/x-data-grid";
 import { useDispatch, useSelector } from "react-redux";
 import Navigation from "./Navigation";
+import moment from "moment";
 
 export default function Results() {
   const dispatch = useDispatch();
   const { userTests } = useSelector((state) => state.usertests);
   const navigate = useNavigate();
+  const theme = useTheme();
 
   useEffect(() => {
     dispatch(getUserTests());
   }, []);
 
-  useEffect(() => {
-    console.log(userTests);
-  }, [userTests]);
 
-  const handleGetTest = (testId = null) => {};
+  const ProgressDiv = styled("div")({
+    width: "100%",
+    border: "1px solid rgba(140, 140, 140, 0.5)",
+    height: "26px",
+    overflow: "hidden",
+    position: "relative",
+    borderRadius: "2px",
+  });
+
+  const ProgressContainer = styled("div")({
+    width: "100%",
+    display: "flex",
+    position: "absolute",
+    lineHeight: "24px",
+    justifyContent: "center",
+    color: theme.palette.mode === "light" ? "black" : "white",
+  });
 
   const columns = [
-    { field: "id", headerName: "ID", width: 50, hide: true },
+    {
+      field: "id",
+      headerName: "ID",
+      width: 50,
+      renderHeader: (p) => (
+        <strong style={{ overflow: "visible" }}>{p.colDef.headerName}</strong>
+      ),
+      hide: true,
+    },
     {
       field: "created_at",
       headerName: "fecha",
       width: 120,
+      renderHeader: (p) => (
+        <strong style={{ overflow: "visible" }}>{p.colDef.headerName}</strong>
+      ),
       valueFormatter: (params) => {
         return new Date(params.value).toLocaleDateString();
       },
@@ -35,6 +63,9 @@ export default function Results() {
       field: "subject_name",
       headerName: "Materia",
       width: 120,
+      renderHeader: (p) => (
+        <strong style={{ overflow: "visible" }}>{p.colDef.headerName}</strong>
+      ),
       valueFormatter: (params) => {
         return params.value ? params.value : "Simulacro";
       },
@@ -44,17 +75,82 @@ export default function Results() {
       headerName: "Puntos",
       width: 120,
     },
+    {
+      field: "progress",
+      headerName: "Progreso",
+      width: 250,
+      renderHeader: (p) => (
+        <strong style={{ overflow: "visible" }}>{p.colDef.headerName}</strong>
+      ),
+      renderCell: (params) => {
+        return params.value !== null ? (
+          <ProgressDiv>
+            <ProgressContainer>
+              {((params.row.grade * 100) / params.row.points).toFixed(2)}%
+            </ProgressContainer>
+            <div
+              style={{
+                height: "100%",
+                maxWidth: `${(
+                  (params.row.grade * 100) /
+                  params.row.points
+                ).toFixed(2)}%`,
+                backgroundColor:
+                  theme.palette.mode === "light"
+                    ? theme.palette.primary.light
+                    : theme.palette.primary.dark,
+              }}
+            />
+          </ProgressDiv>
+        ) : (
+          ""
+        );
+      },
+    },
+    {
+      field: "total",
+      headerName: "Total",
+      width: 100,
+      renderHeader: (p) => (
+        <strong style={{ overflow: "visible" }}>{p.colDef.headerName}</strong>
+      ),
+      renderCell: (params) => {
+        const total = params.row.grade + "/" + params.row.points;
+        return total;
+      },
+    },
+    {
+      field: "duration",
+      headerName: "Tiempo",
+      width: 220,
+      renderHeader: (p) => (
+        <strong style={{ overflow: "visible" }}>{p.colDef.headerName}</strong>
+      ),
+      valueFormatter: (params) => {
+        return params.value !== null
+          ? moment(params.value).format("HH:mm:ss")
+          : "Sin datos";
+      },
+    },
   ];
 
   return (
-    <Box sx={{ minWidth: 200 }}>
-      <DataGrid
-        rows={userTests}
-        columns={columns}
-        pageSize={10}
-        rowsPerPageOptions={[10, 20, 50]}
-      />
-      <Navigation />
-    </Box>
+    <React.Fragment>
+      <Box sx={{ minWidth: 200 }}>
+        <div style={{ height: 720, width: "100%" }}>
+          <DataGrid
+            initialState={{
+              pagination: { paginationModel: { page: 0, pageSize: 10 } },
+            }}
+            localeText={esES.components.MuiDataGrid.defaultProps.localeText}
+            pageSizeOptions={[10, 20, 50]}
+            rows={userTests ?? []}
+            rowHeight={50}
+            columns={columns}
+          />
+          <Navigation />
+        </div>
+      </Box>
+    </React.Fragment>
   );
 }
